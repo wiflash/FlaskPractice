c147d6da78120a4b63bf08ae639069985d535178
Wildan Firdaus <firdaus@alterra.id>
2019-12-17 11:22:44 +0700

    added error handling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-import json
+import json, logging
+from logging.handlers import RotatingFileHandler
 
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, catch_all_404s=True)
 
 
 @app.route("/")
@@ -18,18 +19,36 @@ app.register_blueprint(blueprint_user, url_prefix="/user")
 
 @app.after_request
 def after_request(response):
-    if request.method == "GET":
-        app.logger.warning("REQUEST_LOG\t%s", json.dumps({
-            "request": request.args.to_dict(),
+    try:
+        request_data = request.get_json()
+    except:
+        request_data = request.args.to_dict()
+    if response.status_code == 200:
+        app.logger.info("REQUEST_LOG\t%s", json.dumps({
+            "method": request.method,
+            "code": response.status,
+            "request": request_data,
             "response": json.loads(response.data.decode("utf-8"))
         }))
     else:
-        app.logger.warning("REQUEST_LOG\t%s", json.dumps({
-            "request": request.get_json(),
+        app.logger.error("REQUEST_LOG\t%s", json.dumps({
+            "method": request.method,
+            "code": response.status,
+            "request": request_data,
             "response": json.loads(response.data.decode("utf-8"))
         }))
     return response
 
 
 if __name__ == "__main__":
+    log_path = "/storage/log"
+    logging.basicConfig(level=logging.INFO)
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s"
+    )
+    log_handler = RotatingFileHandler(
+        "%s/%s" %(app.root_path, log_path+"/app.log"), maxBytes=100000, backupCount=10
+    )
+    log_handler.setFormatter(formatter)
+    app.logger.addHandler(log_handler)
     app.run(debug=True)
