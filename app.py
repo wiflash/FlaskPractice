e7c09aee6668cbddce662321c8cc27f312fdbd06
Wildan Firdaus <firdaus@alterra.id>
2019-12-17 10:08:24 +0700

    added log decorator

--- app.py
+++ app.py
@@ -11,8 +11,18 @@ api = Api(app)
 def home():
     return "<h1>Challenge 1</h1>"
 
+
 from blueprints.user.resources import blueprint_user
 app.register_blueprint(blueprint_user, url_prefix="/user")
+
+
+@app.after_request
+def after_request(response):
+    app.logger.warning("REQUEST_LOG\t%s", json.dumps({
+        "request": request.get_json(),
+        "response": json.loads(response.data.decode("utf-8"))
+    }))
+    return response
 
 
 if __name__ == "__main__":
